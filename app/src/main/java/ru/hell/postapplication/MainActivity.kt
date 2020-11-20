afb70b8bf21768e827573d6f9ed3855ddbe12070
Svinsborg <viktorov.a.m@mail.ru>
2020-11-20 23:26:34 +0300

    Add Date algoritm

--- app/src/main/java/ru/hell/postapplication/MainActivity.kt
+++ app/src/main/java/ru/hell/postapplication/MainActivity.kt
@@ -13,8 +13,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        val moscowGMT = 10800L
+        val startDate: Long = 1605914700 // 20 November 2020 г., 23:25:00
+        val timestamp = System.currentTimeMillis()/1000 + moscowGMT
+        val timePost = frendlyTime((timestamp - startDate))
 
-        val post = Post(1,"Andy","Some very long text for testing an application for Android","3 aug 2020",false,13,2,1)
+
+
+
+
+
+        val post = Post(1,"Andy",
+                "Some very long text for testing an application for Android",
+                "$timePost",
+                false,
+                13,
+                2,
+                1)
 
         val author: TextView = findViewById(R.id.author)
         val dateCreated: TextView = findViewById(R.id.created)
@@ -36,3 +51,91 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+fun frendlyTime(time:Long): String {
+
+    var stringTime: String
+    when (time){
+        in 0..59L -> stringTime = "меньше минуты назад"
+        in 0..3600L -> stringTime = oneHour(time)
+        in 3600..7199 -> stringTime = "час назад"
+        in 7200..10799 -> stringTime = "2 часа назад"
+        in 10800..86399 -> stringTime = "несколько часов назад"
+        in 86400..604799 -> stringTime = oneDay(time)
+        in 604800..2678399 -> stringTime = oneWeek(time)
+        in 2678400..31622399 -> stringTime = oneMonth(time)
+        in 31622399..157679999 -> stringTime = oneYear(time)
+        in 157680000..Long.MAX_VALUE -> stringTime = "A long time ago, in a galaxy far, far away…"
+        else -> stringTime = "Неверные данные"
+    }
+    return stringTime
+}
+
+fun oneHour(sec:Long): String {
+    var stringTime: String
+
+    val time = ( sec / 60 )
+    val lastСharacter: Long = time%10
+    if(time in 5..20){
+        stringTime = "$time минут назад"
+    } else {
+        if (lastСharacter == 1L ) {
+            stringTime = "$time минуту назад"
+        } else if (lastСharacter in 5..9 || lastСharacter == 0L) {
+            stringTime = "$time минут назад"
+        } else{
+            stringTime = "$time минуты назад"
+        }
+    }
+    return stringTime
+}
+
+fun oneDay(sec:Long): String {
+    var stringTime: String
+    val time = ( sec / 60 / 60 / 24 )
+    stringTime ="$time дней назад"
+    return stringTime
+}
+
+fun oneWeek(sec:Long): String {
+    var stringTime: String
+    when (val time = (sec / 60 / 60 / 24 / 7)) {
+        1L -> stringTime = "$time неделю назад"
+
+        in 2..4 ->  stringTime = "$time недели назад"
+
+        else -> stringTime ="$time недель назад"
+        }
+    return stringTime
+}
+
+fun oneMonth(sec:Long): String {
+    var stringTime: String
+    when (val time = ( sec / 60 / 60 / 24 / 30 )) {
+        in 2..4 -> {
+            stringTime ="$time месяца назад"
+        }
+        in 5..12 -> {
+            stringTime ="$time месяцев назад"
+        }
+        else -> {
+            stringTime ="$time месяц назад"
+        }
+    }
+    return stringTime
+}
+
+fun oneYear(sec:Long): String {
+    var stringTime: String
+    when (val time = ( sec / 60 / 60 / 24 / 30 / 12 )) {
+        1L -> {
+            stringTime = "$time год назад"
+        }
+        in 2..4 -> {
+            stringTime = "$time года назад"
+        }
+        else -> {
+            stringTime = "$time лет назад"
+        }
+    }
+    return stringTime
+}
